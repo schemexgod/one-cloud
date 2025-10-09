e19d956d4274a7e32f4f09179e81e404930b82a9
Eric Eng <eric@rabbittt.com>
2025-10-09 16:18:53 -0400

    - wip templates

--- frontend/src/js/one-shot_tests.js
+++ frontend/src/js/one-shot_tests.js
@@ -6,3 +6,39 @@
 function createFromTemplate() {
 
 }
+
+/**
+ * Static helper functions
+ */
+const Helper = {
+    /**
+     * Creates an HTMLElement from a premade template
+     * @param {string} templateId  
+     * @param {object} props 
+     * @returns {HTMLElement}
+     */
+    // function createElementFromTemplate(templateId, props) 
+}
+
+
+/**
+ * A simple Card layout
+ * @param {string?} title 
+ * @param {string?} subtitle 
+ * @param {string?} imageUrl 
+ * @returns {HTMLElement}
+ */
+export const CardA = (title, subtitle, imageUrl) => {
+    
+}
+
+/**
+ * Hero section layout with a large image background and text
+ * @param {string?} title 
+ * @param {string?} subtitle 
+ * @param {string?} backgroundImage 
+ * @returns {HTMLElement}
+ */
+export const HeroA = (title, subtitle, backgroundImage) => {
+    
+}
