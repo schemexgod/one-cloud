46c09391fc1297351cb1938d1dc0338cdeeb9449
Eric Eng <eric@createwithplay.com>
2025-10-27 22:16:13 -0400

    - works

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -9,11 +9,40 @@ const app = appOneShot
 // Check if signed in
 const auth = getAuth(app);
 console.log('user', auth.currentUser)
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
     if (!user) {
         loadSignin()
     } else {
         console.log('user', auth.currentUser)
+        const jwtId =  await auth.currentUser.getIdToken()
+        const testEndPoint = 'https://us-central1-oneshot-c5e23.cloudfunctions.net/dbConnect'
+        const postData = {
+            title: 'My New Post',
+            body: 'This is the content of my new post.',
+            userId: "some-uid",
+            endpoint: "create-db"
+        };
+
+        fetch(testEndPoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwtId}`
+            },
+            body: JSON.stringify(postData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('New post created:', data);
+            })
+            .catch(error => {
+                console.error('Error creating post:', error);
+            });
     }
 })
 
