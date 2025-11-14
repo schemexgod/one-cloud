93946c9c3f60ac4a3621766ea65b40330f5015c5
Eric Eng <eric@createwithplay.com>
2025-11-14 14:13:40 -0500

    - wip custom framework

--- frontend/src/js/pages/testJsx.jsx
+++ frontend/src/js/pages/testJsx.jsx
@@ -1,22 +1,30 @@
-import { jsxToDomNode } from 'play-web-ui'
-
+import PlayWebUI from 'play-web-ui'
+console.log('PlayWebUI', PlayWebUI)
 const TestCustomEl = (innProps) => {
   return (
-    <main>
-      <h3 custom-prop="hi">i am custom</h3>
-      {innProps?.children}
-    </main>
+    <>
+      <main>
+        <h3 custom-prop="hi">i am custom</h3>
+        {innProps?.children}
+      </main>
+      <section>new section</section>
+    </>
 
   )
 }
 
 export const testJSX = (
-  <div id="hello2">
-    <span>ttddd</span>
-    <div>
-      <TestCustomEl custom-in-prop="fine">ddd</TestCustomEl>
-    </div>
-  </div>
+  <>
+  <div>1</div>
+  <div>2</div>
+  </>
+  // <div id="hello2">
+  //   <span>ttddd</span>
+  //   <div>
+  //     <TestCustomEl custom-in-prop="fine">ddd</TestCustomEl>
+  //   </div>
+  // </div>
 )
 
+console.log('testJSX', testJSX)
 document.body.appendChild(testJSX)
--- frontend/src/js/play-web-ui/index.js
+++ frontend/src/js/play-web-ui/index.js
@@ -1,53 +1,10 @@
-export { view, View } from './src/View'
+import * as translator from './src/jsx-translator'
+import * as View from "./src/View";
 
-/**
- * 
- * @param {string | Node} tag 
- * @param {*} props 
- * @param  {...any} children 
- * @returns 
- */
-export const jsxToDomNode = (tag, props, ...children) => {
-  console.log('build1 --', tag, props, children)
+export * from './src/jsx-translator'
+export * from "./src/View";
+export default {
+  ...View,
+  ...translator
 
-  if (typeof tag === 'function') {
-    props.children = children
-    tag = tag(props)
-  }
-
-  if (typeof tag === 'object') {
-    return tag
-  }
-  
-  if (!typeof tag === 'string') {
-    return
-  }
-  const element = document.createElement(tag);
-
-
-  Object.entries(props || {}).forEach(([key, value]) => {
-    // Block some internal props
-    if (key.startsWith('__')) { return }
-
-    if (key === 'className') { // Handle className for HTML classes
-      element.setAttribute('class', value);
-    } else if (key.startsWith('on') && typeof value === 'function') { // Handle event listeners
-      const eventName = key.toLowerCase().substring(2);
-      element.addEventListener(eventName, value);
-    } else {
-      element.setAttribute(key, value);
-      console.log('--  attr', key, value)
-    }
-  });
-
-  children.flat().forEach(child => {
-    console.log('--  children', child, typeof child)
-    if (typeof child === 'string' || typeof child === 'number') {
-      element.appendChild(document.createTextNode(child));
-    } else if (child instanceof Node) {
-      element.appendChild(child);
-    }
-  });
-
-  return element;
-};
+}
